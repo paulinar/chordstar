9114970c5aba6d0d688c659d854a92f244e517ba
Paulina Ramos <m.paulina.ramos@gmail.com>
2014-12-06 21:46:22 -0800

    Set up new fragments, add assets and styling

--- Chordstars/app/src/main/java/guitarheroes/com/chordstars/MainActivity.java
+++ Chordstars/app/src/main/java/guitarheroes/com/chordstars/MainActivity.java
@@ -1,15 +1,17 @@
 package guitarheroes.com.chordstars;
 
 import android.app.Activity;
-import android.app.FragmentTransaction;
-import android.support.v4.app.Fragment;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -17,53 +19,57 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
 
-            SelectSongFragment firstFragment = new SelectSongFragment();
+    }
 
-            FragmentTransaction t = getFragmentManager().beginTransaction();
-            t.add(R.id.container, firstFragment);
-            t.commit();
+    public void onOpenWebBrowser(View v) {
+        Intent webPageIntent = new Intent(Intent.ACTION_VIEW);
+        webPageIntent.setData(Uri.parse("https://play.google.com/store/music"));
+
+        try {
+            startActivity(webPageIntent);
+        } catch (ActivityNotFoundException ex) {
         }
     }
 
+    public void selectFrag(View view) {
+        Fragment fr;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+//        if(view == findViewById(R.id.button2)) {
+//            fr = new FragmentTwo();
+//
+//        }else {
+//            fr = new FragmentOne();
+//        }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        if (view == findViewById(R.id.selectSongBtn)) {
+            fr = new SelectSongFrag2();
+            findViewById(R.id.selectSongBtn).setVisibility(View.GONE);
+            findViewById(R.id.downloadMoreBtn).setVisibility(View.GONE);
+            findViewById(R.id.musicalNote).setVisibility(View.GONE);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+//            Button playOnGuitarBtn = (Button) findViewById(R.id.selectSongBtn);
+//            playOnGuitarBtn.setTag(1);
+//            playOnGuitarBtn.setText("PLAY ON GUITAR");
+//
+//            Button listenToSong = (Button) findViewById(R.id.downloadMoreBtn);
+//            listenToSong.setTag(1);
+//            listenToSong.setText("LISTEN TO SONG");
+
+        } else if (view == findViewById(R.id.downloadMoreBtn)) {
+            fr = new SelectSongFrag1();
+            startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://www.google.com")));
+        } else {
+            fr = new SelectSongFrag1(); // TODO: OPEN CHROME
         }
 
-        return super.onOptionsItemSelected(item);
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, fr);
+        fragmentTransaction.commit();
+
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 }
