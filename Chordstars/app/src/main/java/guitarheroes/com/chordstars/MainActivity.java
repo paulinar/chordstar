54abaa20b7e8bc027b6ca6a312bdddfc0994683f
Paulina Ramos <m.paulina.ramos@gmail.com>
2014-12-07 01:26:44 -0800

    User flow up to Session Summary works! Including 5-second delay for ToqActivity

--- Chordstars/app/src/main/java/guitarheroes/com/chordstars/MainActivity.java
+++ Chordstars/app/src/main/java/guitarheroes/com/chordstars/MainActivity.java
@@ -1,17 +1,30 @@
 package guitarheroes.com.chordstars;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+import android.os.*;
 
 
 public class MainActivity extends Activity {
@@ -22,7 +35,25 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
+//        hiddenBtn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                try {
+//                    Thread.sleep(2000);
+//                    Fragment fr = new EndSessionFrag();
+//                    FragmentManager fm = getFragmentManager();
+//                    FragmentTransaction fragmentTransaction = fm.beginTransaction();
+//                    fragmentTransaction.setCustomAnimations(R.animator.slide_in_left, R.animator.slide_in_left);
+//                    fragmentTransaction.replace(R.id.fragment_place, fr);
+//                    fragmentTransaction.commit();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+
     }
+
 
     public void onOpenWebBrowser(View v) {
         Intent webPageIntent = new Intent(Intent.ACTION_VIEW);
@@ -30,7 +61,8 @@ public class MainActivity extends Activity {
 
         try {
             startActivity(webPageIntent);
-        } catch (ActivityNotFoundException ex) {
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
@@ -53,7 +85,7 @@ public class MainActivity extends Activity {
         FragmentManager fm = getFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
 
-        fragmentTransaction.setCustomAnimations(R.animator.slide_in_left, R.animator.slide_in_left);
+        fragmentTransaction.setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_left);
         fragmentTransaction.replace(R.id.fragment_place, fr);
         fragmentTransaction.commit();
 
@@ -61,9 +93,21 @@ public class MainActivity extends Activity {
 
     public void endSession(View view) {
         Fragment fr;
+        fr = new SessionSummaryFrag();
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_left);
+        fragmentTransaction.replace(R.id.fragment_place, fr);
+        fragmentTransaction.commit();
+
+    }
+
+    public void restartSession(View view) {
+        Fragment fr;
         fr = new EndSessionFrag();
         FragmentManager fm = getFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_left);
         fragmentTransaction.replace(R.id.fragment_place, fr);
         fragmentTransaction.commit();
 
@@ -74,9 +118,27 @@ public class MainActivity extends Activity {
         fr = new PlayOnGuitarFrag();
         FragmentManager fm = getFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_left);
         fragmentTransaction.replace(R.id.fragment_place, fr);
         fragmentTransaction.commit();
 
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                Fragment endSessionFrag = new EndSessionFrag();
+                FragmentManager newFM = getFragmentManager();
+                FragmentTransaction newFT = newFM.beginTransaction();
+                newFT.setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_left);
+                newFT.replace(R.id.fragment_place, endSessionFrag);
+                newFT.commit();
+            }
+        }, 5000);
+
+        // UNCOMMENT WHEN SARAH IMPLEMENTS TOQACTIVITY CLASS
+//        Intent intent = new Intent(MainActivity.this, ToqActivity.class);
+//        startActivity(intent);
     }
 
 }
--- Chordstars/app/src/main/java/guitarheroes/com/chordstars/SessionSummaryFrag.java
+++ Chordstars/app/src/main/java/guitarheroes/com/chordstars/SessionSummaryFrag.java
@@ -6,13 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class EndSessionFrag extends Fragment {
+public class SessionSummaryFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         // called by Android system so that fragment creates its UI; returns a View component placed in <fragment> element of layout
 
         return inflater.inflate(
-                R.layout.end_session_frag, container, false); // inflate layout of XML file and return its view
+                R.layout.session_summary_frag, container, false); // inflate layout of XML file and return its view
     }
 }
